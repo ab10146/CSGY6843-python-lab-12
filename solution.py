b243e00fc97b8e435ba6d958155978fbd3031387
ab10146 <98478110+ab10146@users.noreply.github.com>
2022-02-18 12:57:56 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,3 @@
-# import socket module
 from socket import *
 # In order to terminate the program
 import sys
@@ -8,45 +7,36 @@ def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
     serverSocket.bind(("", port))
-    # Fill in start
-
-    # Fill in end
+    serverSocket.listen(1)
 
     while True:
         # Establish the connection
-        # print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept()  # https://docs.python.org/3/library/socket.html#socket.socket.accept
         try:
 
             try:
-                message = connectionSocket.recv()  # https://docs.python.org/3/library/socket.html#socket.socket.recv
+                message = connectionSocket.recv(1024)  # https://docs.python.org/3/library/socket.html#socket.socket.recv
                 filename = message.split()[1]
                 f = open(filename[1:])
                 outputdata = f.read()
 
                 # Send one HTTP header line into socket.
-                # Fill in start
-                connectionSocket.send("HTTP/1.1 200 OK\r\n")
-                # Fill in end
+                status = "HTTP/1.1 200 OK\r\n"
+                connectionSocket.send(status.encode())
 
                 # Send the content of the requested file to the client
-                for i in range(0, len(outputdata)):
+                for i in range(0, len(outputdata)-1):
                     connectionSocket.send(outputdata[i].encode())
 
                 connectionSocket.send("\r\n".encode())
                 connectionSocket.close()
             except IOError:
                 # Send response message for file not found (404)
-                # Fill in start
-                connectionSocket.send("HTTP/1.1 404 Not Found\r\n")
-
-                # Fill in end
+                status = "HTTP/1.1 404 Not Found\r\n"
+                connectionSocket.send(status.encode())
 
                 # Close client socket
-                # Fill in start
                 connectionSocket.close()
-
-                # Fill in end
 
         except (ConnectionResetError, BrokenPipeError):
             pass
